6cc52fb06636ca6c00df7f1bac2e52b79b672c2c
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2017-01-29 05:23:11 -0400

    Users CRUD Controller with basic validation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $response = User::all();
+
+        return response()->json($response);
+    }
+
+    public function get($id)
+    {
+        $response = User::find($id);
+
+        return response()->json($response);
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $input = $request->only('firstname', 'lastname', 'email', 'password');
+        $response = User::create($input);
+
+        return response()->json($response);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|unique:users,email,' . $id . '|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $input = $request->only(['firstname', 'lastname', 'email', 'password']);
+        $input = array_filter($input, 'strlen');
+
+        try {
+            $user = User::findOrFail($id);
+            $user->update($input);
+
+            $response = ['msg' => 'User was updated!'];
+        } catch (\Exception $e) {
+            $response = [
+                'devMessage' => $e->getMessage(),
+                'userMessage' => 'An error has ocurred! The user has not been updated.',
+                'status' => 404
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+
+            $response = [
+                'devMessage' => 'The user was deleted!',
+                'userMessage' => 'The user was deleted!',
+                'status' => 200
+            ];
+        } catch (\Exception $e) {
+            $response = [
+                'devMessage' => $e->getMessage(),
+                'userMessage' => 'An error has ocurred! The user has not been deleted.',
+                'status' => 404
+            ];
+        }
+
+        return response()->json($response);
+    }
+
+}
